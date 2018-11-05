942ddf57dc72f2650a5fd4d87667b78165fbf937
Weslan <weslanra@dcomp.ufs.br>
2018-11-05 18:07:54 +0000

    Project and site initiation for visualization

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # e-smart
+##https://e-smart-weslan.c9users.io/
 Database translator for JSON or sending to bases ORION - FIWARE
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="author" content="Weslan Rezende Alves">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="shortcut icon" href="imagem/icon.png" type="image/x-icon"/>
+    </head>
+    <body>
+        <h3>TESTE</h3>
+    </body>
+</html>
